10832ca1b9ec5c8a61db956f9202a6939ee0f8a0
yquintero.andes <yquintero.andes@gmail.com>
2024-05-15 18:15:24 -0500

    rest

---
+++ src/Componentes/CompCrearClientes.jsx
@@ -0,0 +1,77 @@
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+const URL = 'http://localhost:7000/api/clientes/';
+
+const CompCrearClientes = () => {
+
+    const [nombres, setNombres] = useState('');
+    const [apellidos, setApellidos] = useState('');
+    const [cedula, setCedula] = useState('');
+    const [correo, setCorreo] = useState('');
+    const [telefono, setTelefono] = useState('');
+    const [direccion, setDireccion] = useState('');
+    const navigate = useNavigate();
+
+    // creamos la funcion guardar 
+    const guardarClientes = async (e) => {
+        e.preventDefault();
+        await axios.post(URL, {
+            nombres: nombres, apellidos: apellidos, cedula: cedula, correo: correo,
+            telefono: telefono, direccion: direccion
+        })
+        navigate('/clientes')
+    }
+
+    return (
+
+        <div>
+            <h3>Formulario de Guardar Cliente</h3>
+            <form onSubmit={guardarClientes}>
+             
+             <div className=" mb-3">
+             <label className="form-label">Nombre Clientes </label>
+             <input value = {nombres}  onChange={(e) => setNombres(e.target.value)} 
+             type = 'text' className="form-control"/>
+              </div>
+
+              <div className=" mb-3">
+             <label className="form-label">Apellidos cliente </label>
+             <input value = {apellidos}  onChange={(e) => setApellidos(e.target.value)} 
+             type = 'text' className="form-control"/>
+              </div>
+
+              <div className=" mb-3">
+             <label className="form-label">Documento </label>
+             <input value = {cedula}  onChange={(e) => setCedula(e.target.value)} 
+             type = 'number' className="form-control"/>
+              </div>
+
+              <div className=" mb-3">
+             <label className="form-label">Correo </label>
+             <input value = {correo}  onChange={(e) => setCorreo(e.target.value)} 
+             type = 'text' className="form-control"/>
+              </div>
+
+              <div className=" mb-3">
+             <label className="form-label">Telefono </label>
+             <input value = {telefono}  onChange={(e) => setTelefono(e.target.value)} 
+             type = 'number' className="form-control"/>
+              </div>
+
+              <div className=" mb-3">
+             <label className="form-label"> Direccion </label>
+             <input value = {direccion}  onChange={(e) => setDireccion(e.target.value)} 
+             type = 'text' className="form-control"/>
+              </div>
+
+              <button type="submit" className="btn btn-primary"><i className="fa-solid fa-floppy-disk"></i></button>
+
+            </form>
+        </div>
+
+    )
+}
+
+export default CompCrearClientes;
---
+++ src/Componentes/CompMostrarclientes.jsx
@@ -0,0 +1,71 @@
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+const URL = 'http://localhost:7000/api/clientes/';
+
+const CompMostrarClientes = () => {
+
+    const [clientes, setClientes] = useState([])
+
+     useEffect(() => {
+        getclientes()
+     }, []);
+
+
+    // funcion para mostrar los clientes 
+    const getclientes = async () => {
+        const resul = await axios.get(URL)
+        setClientes(resul.data);
+    }
+
+    // funcion eliminar clientes
+    const eliminarClientes = async (id) => {
+     await axios.delete(`${URL}${id}`)
+      getclientes();
+    }
+
+
+    return(
+
+         <div className='container'>
+            <div className='row'>
+                <div className='col'>
+                    <Link to='/clientes/agregar' className='btn btn-success mt-2 mb-2'><i className="fa-solid fa-user-plus"></i> </Link>
+                    <div class="table-responsive"> 
+                    <table className="table">
+                    <thead className='table-light'>
+                        <tr>
+                            <th>Nombres Cliente</th>
+                            <th>Apellidos Cliente</th>
+                            <th> Documento</th>
+                            <th> Correo</th>
+                            <th> Telefono</th>
+                            <th> Direccion</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead> 
+                    <tbody>
+                          {clientes.map( (cliente, index) =>(
+                           <tr key = {index}>
+                           <td> {cliente.nombres} </td>
+                           <td> {cliente.apellidos} </td>
+                           <td> {cliente.cedula} </td>
+                           <td> {cliente.correo} </td>
+                           <td> {cliente.telefono} </td>
+                           <td> {cliente.direccion} </td>
+                           <td>
+                           <Link to= {`/clientes/editar/${cliente._id}`} className='btn btn-primary mt-2 mb-2'><i className="fa-solid fa-file-pen"></i> </Link>
+                           <button onClick={()=> eliminarClientes(cliente._id)} className="btn btn-danger"><i className="fa-solid fa-trash"></i></button>
+                          </td>
+                          </tr>
+                        ))}
+                    </tbody>
+                    </table>
+                    </div>
+                </div>
+            </div>
+     </div>
+    )
+}
+export default CompMostrarClientes;
